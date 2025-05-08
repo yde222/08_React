73a5c28d1820df7ae9c86d5a969f068460b4406e
seungje_yu <coffit23@gmail.com>
2025-05-09 08:30:03 +0900

    folder move

---
+++ 00_YUSEUNGJE/module01-intro/src/pages/_app.js
@@ -0,0 +1,29 @@
+/*
+ _app.js 파일은 모든 페이지에서 공통적으로 사용되는 기능을 정의하는 파일이다.
+ 이 파일은 페이지 전체에 공통 기능을 정의하고, 페이지 전화시 공통 기능을 유지하는데 사용된다.
+ 예를 들어, 모든 페이지에 사용되는 공통 스타일, 라우팅, 데이터로드 등을 정의할 수 있다.
+*/
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* 전역 컴포넌트 (모든 페이지에 표시) */}
+      <header>
+        <nav>
+          <h3>공통 Header</h3>
+        </nav>
+      </header>
+
+      {/* 현재 페이지 컴포넌트 렌더링 */}
+      <Component {...pageProps} />
+
+      {/* 전역 푸터 */}
+      <footer>
+        <p>Encore</p>
+        <p>encore@encore.com</p>
+      </footer>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ 00_YUSEUNGJE/module01-intro/src/pages/about.js
@@ -0,0 +1,12 @@
+import SimpleComponent from "@/components/SimpleComponent";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About 페이지</h1>
+      <p>이것은 About 페이지입니다.</p>
+      <hr />
+      <SimpleComponent />
+    </div>
+  );
+}
