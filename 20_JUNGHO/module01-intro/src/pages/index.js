1ffb13f6440fe137e580c279505634405cdaae81
jungho <kiik52@naver.com>
2025-05-08 18:07:09 +0900

    commit

---
+++ 20_JUNGHO/module01-intro/src/pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>홈페이지</h1>
+      <p>환영합니다. Next.js 어플리케이션의 메인페이지 입니다.</p>
+      <nav>
+        <ul>
+          <li>hello world Next</li>
+          <li>
+            <a href="/about">hello world About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
