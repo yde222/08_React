ecf0b3db69116423e10ad23ffc08e97a0496059c
jieun <yudaeun222@gmail.com>
2025-05-09 12:13:48 +0900

    create

---
+++ 08_YUJIEUN/08_React/module02-jsx-component/src/app/chap01/section03/page.jsx
@@ -0,0 +1,10 @@
+import JSXRules from "@/app/chap01/section03/jsx-rules";
+export default function Section03() {
+    return (
+        <div>
+        {/* 컴포넌트 추가 */}
+        <JSXRules />
+        </div>
+    );
+    }
+    
