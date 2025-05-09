227b7f58103b43ef6cd517166ba7b62cea87a5dd
jieun <yudaeun222@gmail.com>
2025-05-09 10:19:27 +0900

    update

--- 08_YUJIEUN/08_React/module02-jsx-component/src/app/chap01/section01/page.jsx
+++ 08_YUJIEUN/08_React/module02-jsx-component/src/app/chap01/section01/page.jsx
@@ -3,8 +3,8 @@ import JSXIntro from "@/component/chap01/section01/JSXIntro";
 export default function Section01() {
   return (
     <div>
-     {/*  컴포넌트 추가 */}
-     <JSXIntro />
+      {/* 컴포넌트 추가 */}
+      <JSXIntro />
     </div>
   );
 }
--- 08_YUJIEUN/08_React/module02-jsx-component/src/component/chap01/section01/JSXIntro.jsx
+++ 08_YUJIEUN/08_React/module02-jsx-component/src/component/chap01/section01/JSXIntro.jsx
@@ -1,32 +1,25 @@
-export default function JSXIntro() {
-    
-    
-    return (
-     <div>
-      <h1>JSX Intro</h1>
-        <h2>
-        JSX는 JavaScript XML의 약자로, JavaScript에서 HTML을 작성할 수 있는 문법입니다.
-        </h2>
-        <p>
-            {name}는 JSX를 사용하여 HTML을 작성할 수 있습니다.
-        </p>
-        <h2>h</h2>
-        <div>
-            <h2>JSX는 JavaScript XML의 약자로, JavaScript에서 HTML을 작성할 수 있는 문법입니다.</h2>
-            <p>JSX는 JavaScript와 HTML을 혼합하여 사용할 수 있는 문법입니다.</p>
-            <p>JSX는 React에서 사용되는 문법으로, React 컴포넌트를 작성할 때 사용됩니다.</p>
-            <p>JSX는 JavaScript와 HTML을 혼합하여 사용할 수 있는 문법입니다.</p>
-            <div>
-                <h2>Virtual DOM과의 관계</h2>
-                <p>
-                    JSX로 작성된 코드는 React 요소로 변환되고,
-                    이 요소들이 Virtual DOM을 구성하게된다.
-                    React는 상태 변경 시 Virtual DOM을 효율적으로 업데이트한다.
-                </p>
-                <img src="https://velog.velcdn.com/images/ksykma/post/824b6071-2888-4613-84b8-1a272a09224c/image.png" width={"600px"} height={"300px"}/>
-            </div>
-        </div>
-    </div>
-    );
+/*
+JSX란?
+javascript xml의 합성어로, javascript 확장 문법이다.
+이를 사용하지 않으면 가상 돔을 생성하기 위해서 createElement를 이용하게 된다. 
+이렇게 createElement를 통해 엘리먼트를 정의하면 복잡하기도 하고 가독성도 좋지 않다.
+그래서 리액트 팀은 ReactElement를 정의하는 간편한 방법으로 JSX문법을 제공한다.
+자바스크립트를 확장한 문법으로, ReactElement를 xml문법 형식으로 정의할 수 있는 방법이다.
 
+단, JSX는 공식적인 자바스크립트 문법이 아니다. 바벨이라는 트랜스 파일링 툴이 필요하다.
+바벨은 ES6+, ES next 문법을 ES5문법으로 변환해 주는 도구이다.
+추가로 JSX를 순수 리액트로 변환해 주는 기능이 포함되었다.(바벨이 JSX 처리 표준으로 사용된다.)
+*/
+
+export default function JSXIntro() {
+  const name = "홍길동"; // javascript 변수
+
+  return (
+    <div>
+      <h1>JSX의 개념과 역할</h1>
+      <h2>안녕하세요!</h2>
+      <p>{name}님 환영합니다.</p> {/* 중괄호를 사용해 JavaScript 표현식 삽입 */}
+      <p>현재 시간: {new Date().toLocaleTimeString()}</p>
+    </div>
+  );
 }
