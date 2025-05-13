23bb3225df717b52397555a77c8af937b378b7d9
jieun <yudaeun222@gmail.com>
2025-05-13 10:06:18 +0900

    memo

---
+++ 08_YUJIEUN/module04-react-hooks/src/app/chap03/section02/page.jsx
@@ -0,0 +1,11 @@
+import UseRefValueKeep from "@/components/chap03/section02/UseRefValueKeep";
+
+function Section02() {
+  return (
+    <div>
+      <UseRefValueKeep />
+    </div>
+  );
+}
+
+export default Section02;
---
+++ 08_YUJIEUN/module04-react-hooks/src/components/chap04/section02/UserCallback.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState, useCallback } from "react";
+
+export default function UserCallback() {
+  return (
+    <div>
+      <h2>UseCallback</h2>
+    </div>
+  );
+}
